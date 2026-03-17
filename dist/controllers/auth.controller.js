bc722b6b675c741032e1161c6425fac22598e248
Jerome Mondol <jeromemondol11@gmail.com>
2026-03-18 02:19:51 +0600

    Stop tracking the dist folder

--- dist/controllers/auth.controller.js
+++
@@ -1,7 +0,0 @@
-import catchAsync from '../utils/catchAsync.js';
-// register user controller
-export const registerUser = catchAsync(async (req, res) => {
-    const userData = req.body;
-    console.log('Received user data:', userData);
-    res.send(userData);
-});
--- dist/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import multer from 'multer';
-import { registerUser } from "../controllers/auth.controller.js";
-const router = Router();
-const upload = multer();
-// create user in DB
-router.post('/register', upload.none(), registerUser);
-export default router;
